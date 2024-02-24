1e496a3ec038afd17de7497dab9114edeef5c9ef
Menaki <151349693+Soylamango@users.noreply.github.com>
2024-02-24 03:43:38 -0400

    v1

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -9,6 +9,39 @@
 const fs = require('fs');
 const { Microsoft, Mojang } = require('minecraft-java-core');
 const { ipcRenderer } = require('electron');
+const clientId = '1207516304857235546';
+const DiscordRPC = require('discord-rpc');
+const RPC = new DiscordRPC.Client({ transport: 'ipc'});
+
+DiscordRPC.register(clientId);
+
+async function setActivity() {
+   if (!RPC) return;
+   RPC.setActivity({
+       details: `Luxfiro Client`,
+       state: `Launcher de Eventos Publicos`,
+       startTimestamp: Date.now(),
+       largeImageKey: 'https://i.pinimg.com/236x/19/09/67/190967de265ea709ab46020f6128a83d.jpg',
+       largeImageText: `Minecraft Launcher`,
+       instance: false,
+       buttons: [
+           {
+               label: `Discord`,
+               url: `https://discord.gg/udUkgYvmWB `,
+           }
+       ]
+   });
+};
+
+RPC.on('ready', async () => {
+   setActivity();
+
+   setInterval(() => {
+       setActivity();
+   }, 86400 * 1000);
+});
+
+RPC.login({ clientId }).catch(err => console.error(err));
 
 import { config, logger, changePanel, database, addAccount, accountSelect } from './utils.js';
 import Login from './panels/login.js';
